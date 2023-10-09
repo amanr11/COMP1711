4f1eb5e5cb892160789358515e97724e0ec3a737
amanr11 <146495117+amanr11@users.noreply.github.com>
2023-10-09 15:58:38 +0000

    monday sesh

---
+++ week1_c_bootcamp1/factorial.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a,fac;
+    printf("Enter integer: ");
+    scanf("%d", &a);
+    for (i=0 ; i<=a ;i++){
+        fac=fac*i;
+        
+    }
+    printf(fac);
+}
