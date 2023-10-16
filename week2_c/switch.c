a64455aa3056b295d3d4e5752d51d5fd2a3d622d
amanr11 <146495117+amanr11@users.noreply.github.com>
2023-10-16 15:55:17 +0000

    monday sesh

---
+++ week2_c/switch.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a;
+    printf("Enter option: ");
+    scanf("%d",&a);
+    switch (a){ 
+        case 0: printf ("Option 0 has been selected\n");
+        break;
+        case 1: printf ("Option 1 has been selected\n");
+        break;
+        default: printf ("A different option has been selected\n");
+        }
+    }
